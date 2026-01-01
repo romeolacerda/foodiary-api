064a0151444aaafd6653b6490b2736130960fcf4
Romeo <romeolacerdafarias@gmail.com>
2026-01-01 20:13:52 -0300

    feat: get user id through dynamo

--- src/main/functions/auth/preTokenGenerationTrigger.ts
+++ src/main/functions/auth/preTokenGenerationTrigger.ts
@@ -1,11 +1,21 @@
+import 'reflect-metadata';
+
+import { AccountRepository } from '@infra/database/dynamo/repositories/AccountRepository';
+import { Registry } from '@kernel/di/Registry';
 import { PreTokenGenerationV2TriggerEvent } from 'aws-lambda';
 
+const repo = Registry.getInstance().resolve(AccountRepository);
 export async function handler(event: PreTokenGenerationV2TriggerEvent){
+
+  const user = await repo.findEmail(event.request.userAttributes.email);
+
+  console.log(user?.id);
+
   event.response = {
     claimsAndScopeOverrideDetails: {
       accessTokenGeneration: {
         claimsToAddOrOverride: {
-          internalId: 'batatinha123',
+          internalId: user!.id,
         },
       },
     },
