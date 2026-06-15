e29d89acb740ab82af0a28f3f9ff27856b35d7c3
Romeo <romeolacerdafarias@gmail.com>
2026-06-15 20:21:01 -0300

    feat: add custom template to trigger

--- src/main/functions/auth/cognito/customMessageTrigger.ts
+++ src/main/functions/auth/cognito/customMessageTrigger.ts
@@ -1,11 +1,15 @@
+import ForgotPassword from '@infra/emails/template/forgotPassword';
+import { render } from '@react-email/render';
 import { CustomMessageTriggerEvent } from 'aws-lambda';
 
 export async function handler(event: CustomMessageTriggerEvent){
   if(event.triggerSource === 'CustomMessage_ForgotPassword'){
-    const code = event.request.codeParameter;
+    const confirmationCode = event.request.codeParameter;
+
+    const html = await render(ForgotPassword({ confirmationCode }))
 
     event.response.emailSubject = '🍏 foodiary | Recupere sua conta!';
-    event.response.emailMessage = `O seu codigo de recuperação é: ${code}`;
+    event.response.emailMessage = html
   }
   
   return event;
